4c5f1b867b44a67992e5f22ddd2bf76e4db3d9e7
yukinakai <10libra20.y@gmail.com>
2022-02-27 08:13:04 +0900

    tweak

--- scr/components/MemoList.jsx
+++ scr/components/MemoList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function memoList() {
+export default function MemoList() {
   return (
     <View>
       <View style={styles.memoListItem}>
