51be786f63f61b3fddc2659f8493ce0b718b3877
yukinakai <10libra20.y@gmail.com>
2022-03-01 07:38:55 +0900

    create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import MemoListScreen from './scr/screens/MemoListScreen';
 import MemoDetailScreen from './scr/screens/MemoDetailScreen';
+import MemoEditScreen from './scr/screens/MemoEditScreen';
+import MemoCreateScreen from './scr/screens/MemoCreateScreen';
 
 export default function App() {
   return (
     // <MemoListScreen />
-    <MemoDetailScreen />
+    // <MemoDetailScreen />
+    // <MemoEditScreen />
+    <MemoCreateScreen />
   );
 }
