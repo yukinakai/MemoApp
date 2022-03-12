f9d002df974b0db9f2df1b8ca27d95ae58aa7460
yukinakai <10libra20.y@gmail.com>
2022-03-13 08:51:44 +0900

    read firestore data

--- scr/screens/MemoListScreen.jsx
+++ scr/screens/MemoListScreen.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
+import { getAuth } from 'firebase/auth';
+import {
+  getFirestore, collection, getDocs, query, orderBy,
+} from 'firebase/firestore';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
@@ -7,11 +11,31 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
+
   useEffect(() => {
     navigation.setOptions({
       // eslint-disable-next-line react/no-unstable-nested-components
       headerRight: () => <LogOutButton />,
     });
+  }, []);
+
+  useEffect(async () => {
+    const { currentUser } = getAuth();
+    const db = getFirestore();
+    let unsubscribe = () => {};
+    if (currentUser) {
+      try {
+        const q = query(collection(db, `users/${currentUser.uid}/memos`), orderBy('updated_at', 'desc'));
+        const snapshot = await getDocs(q);
+        unsubscribe = snapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+        });
+      } catch (error) {
+        console.log(error);
+        Alert.alert('Error!', error);
+      }
+    }
+    return unsubscribe;
   }, []);
 
   return (
